becbbbc8fa458f867704f10c0216e2bf028e2316
arstanrakhmat <arstanbek.rakh@gmail.com>
2025-10-08 11:40:58 +0600

    Add Dater Formatter

--- app/src/main/java/com/example/suminnotes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/example/suminnotes/presentation/screens/notes/NotesScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -28,12 +29,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.suminnotes.domain.Note
 import com.example.suminnotes.presentation.ui.theme.OtherNotesColors
 import com.example.suminnotes.presentation.ui.theme.PinnedNotesColors
+import com.example.suminnotes.presentation.utils.DateFormatter
 
 @Composable
 fun NotesScreen(
@@ -81,6 +84,7 @@ fun NotesScreen(
                     items = state.pinnedNotes,
                     key = { _, note -> note.id }) { index, note ->
                     NoteCard(
+                        modifier = modifier.widthIn(max = 160.dp),
                         note = note,
                         onNoteClick = {
                             viewModel.processCommand(NotesCommands.EditNote(note))
@@ -226,11 +230,13 @@ fun NoteCard(
         Text(
             text = note.title,
             fontSize = 14.sp,
-            color = MaterialTheme.colorScheme.onSurface
+            maxLines = 1,
+            color = MaterialTheme.colorScheme.onSurface,
+            overflow = TextOverflow.Ellipsis
         )
         Spacer(modifier = modifier.height(8.dp))
         Text(
-            text = note.updatedAt.toString(),
+            text = DateFormatter.formatDateToString(note.updatedAt),
             fontSize = 12.sp,
             color = MaterialTheme.colorScheme.onSurfaceVariant
         )
@@ -238,8 +244,10 @@ fun NoteCard(
         Text(
             text = note.content,
             fontSize = 16.sp,
+            maxLines = 3,
             color = MaterialTheme.colorScheme.onSurface,
-            fontWeight = FontWeight.Medium
+            fontWeight = FontWeight.Medium,
+            overflow = TextOverflow.Ellipsis
         )
     }
 
---
+++ app/src/main/java/com/example/suminnotes/presentation/utils/DateFormatter.kt
@@ -0,0 +1,31 @@
+package com.example.suminnotes.presentation.utils
+
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.concurrent.TimeUnit
+
+object DateFormatter {
+
+    private val millisInHour = TimeUnit.HOURS.toMillis(1)
+    private val millisInDay = TimeUnit.DAYS.toMillis(1)
+    private val formatter = SimpleDateFormat.getDateInstance(DateFormat.SHORT)
+
+    fun formatDateToString(
+        timestamp: Long
+    ): String {
+        val now = System.currentTimeMillis()
+        val diff = now - timestamp
+
+        return when {
+            diff < millisInHour -> "Just now"
+            diff < millisInDay -> {
+                val hours = TimeUnit.MILLISECONDS.toHours(diff)
+                "$hours hours ago"
+            }
+
+            else -> {
+                formatter.format(timestamp)
+            }
+        }
+    }
+}
